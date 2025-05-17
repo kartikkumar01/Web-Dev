f7c70da24815ed826e7cea3badda04a6be0d4e9f
Kartik Kumar <kartikjhaba@gmail.com>
2025-05-17 12:29:31 +0530

    improve names

--- JS PROJECTS/01_todo/script.js
+++ JS PROJECTS/01_todo/script.js
@@ -1,32 +1,36 @@
-const input = document.getElementById('todo-input')
-const addBtn = document.getElementById('add-task-btn')
-const todoListElement = document.getElementById('todo-list')
+const todoInput = document.getElementById('todo-input')
+const addTaskBtn = document.getElementById('add-task-btn')
+const todoLiContainer = document.getElementById('todo-list')
 
-checkTasksInLocalStorageAndDisplay(todoListElement)
+renderTasks(todoLiContainer)
 
-addBtn.addEventListener('click', () => {
-    const taskText = input.value?.trim()
+todoInput.addEventListener('keyup',(event)=>{
+    if(event.key === 'Enter') addTaskBtn.click()
+})
+
+addTaskBtn.addEventListener('click', () => {
+    const taskText = todoInput.value?.trim()
 
     //Security check for the numbers and empty value
     if (taskText !== '' && isNaN(taskText) === true) {
         //TODO
         addTaskToLocalStorage(taskText)
-        displayTemporaryTaskToDOM(taskText, todoListElement)
-        input.value = ''
+        displayTemporaryTaskToDOM(taskText, todoLiContainer)
+        todoInput.value = ''
     } else {
-        input.value = ''
+        todoInput.value = ''
         alert('Input is empty or numeric ...')
     }
 })
 
-todoListElement.addEventListener('click', (event) => {
+todoLiContainer.addEventListener('click', (event) => {
     const tasksListArray = getTasksFromLocalStorage()
 
     //If user clicks on li
     if (event.target.tagName === 'LI') {
         const li = event.target
-        li.classList.toggle('completed')
         const liText = li.getAttribute('data-task')
+        li.classList.toggle('completed')
         //Matching the li with task Object in local storage
         for (let i = 0; i < tasksListArray.length; i++) {
             if (tasksListArray[i].task === liText) {
@@ -38,15 +42,15 @@ todoListElement.addEventListener('click', (event) => {
                 break
             }
         }
-    }//------------If user clicks on delete button--------------
+    }//-----------DELETING THE TASK--------------
     else if (event.target.classList.contains('delete-btn')) {
         const deleteBtn = event.target
         const li = deleteBtn.parentElement
         const liText = li.getAttribute('data-task')
         for (let i = 0; i < tasksListArray.length; i++) {
             if (tasksListArray[i].task === liText) {
-                tasksListArray.splice(i, 1)
-                li.remove()
+                tasksListArray.splice(i, 1) // removing task from localStorage
+                li.remove() // removing task from DOM
                 break
             }
         }
@@ -62,20 +66,20 @@ function createTaskObject(taskText) {
     return task
 }
 
-function checkTasksInLocalStorageAndDisplay(todoListElement) {
+function renderTasks(todoLiContainer) {
     const tasksListArray = getTasksFromLocalStorage()
     tasksListArray.forEach((taskObj) => {
         const li = createLiTaskElement(taskObj.task)
         if (taskObj.completed === true) {
             li.classList.add('completed')
         }
-        todoListElement.append(li)
+        todoLiContainer.append(li)
     })
 }
 
-function displayTemporaryTaskToDOM(taskText, todoListElement) {
+function displayTemporaryTaskToDOM(taskText, todoLiContainer) {
     const li = createLiTaskElement(taskText)
-    todoListElement.append(li)
+    todoLiContainer.append(li)
 }
 
 function createLiTaskElement(taskText) {
