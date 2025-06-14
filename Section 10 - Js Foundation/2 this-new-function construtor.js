400dc5dfbe7744e343ff7c00d0da0caf9dcf948a
Kartik Kumar <kartikjhaba@gmail.com>
2025-06-14 12:13:06 +0530

    Update 2 this-new-function construtor.js

--- Section 10 - Js Foundation/2 this-new-function construtor.js
+++ Section 10 - Js Foundation/2 this-new-function construtor.js
@@ -1,14 +1,15 @@
 /* ek baar spiral me notes bhi dekh lene hai */
-//Function Constructor - It is a function (template) that is used to create multiple objects using new and this keyword.
+//Function Constructor - It is a function (template) that is used to create multiple objects/instances using new and this keyword.
 
 //this(jis) -> It refers to the current context or the object which has called the function.
 
 
-//new -> The new keyword is used to create a new object from a constructor function 
-//It does 3 things when used with constructor function:
+//new -> The new keyword is used to create a new object/instance from a constructor function 
+//It does 4 things when used with constructor function:
 //1) Creates an empty object {}
-//2) Call the function & Sets this of constructor function to point to that object{}
-//3) automatically returns the object
+//2) Call the function & Sets (this) of constructor function to point to that object{}
+//3) Attach the prototype with that newly created object/instance
+//3) automatically returns the object/instance
 
 
 function User(name, age){
